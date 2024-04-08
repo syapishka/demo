e25e46ff8bc3839bf76647df3c0d30e982e84bea
Siarhei <19itsergey@gmail.com>
2024-04-08 21:22:24 +0200

    Initial commit

---
+++ src/main/java/com/example/syasenk/demo/DemobackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.syasenk.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemobackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemobackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/syasenk/demo/DemobackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.syasenk.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemobackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
